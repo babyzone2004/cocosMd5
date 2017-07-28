253cf3f6172a924895b922329f4f2a38598925be
yangweixian <yangweixian@kuaikanmanhua.com>
2017-07-28 18:58:22 +0800

    init

---
+++ build/web-mobile/src/settings.js
@@ -0,0 +1,71 @@
+_CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {},
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ],
+            "6eBWFz0oVHPLIGQKf/9Thu": [
+                "image/default_sprite.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/Scene/home.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/home.fire",
+            uuid: 0
+        },
+        {
+            url: "db://assets/Scene/arena.fire",
+            uuid: 1
+        }
+    ],
+    packedAssets: {
+        "05db87797": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            0,
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ],
+        "096c41d32": [
+            "8c20Sso/ZEn7NUfNSM+EBh",
+            1
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "2dL3kvpAxJu6GJ7RdqJG5J",
+        "d93/KcihJDa7hiBsXbFydZ"
+    ],
+    "md5AssetsMap": {
+        "05/05db87797.json": "2d92e",
+        "09/096c41d32.json": "18124",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/image/default_sprite.png": "68270"
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var fileInline = require('gulp-file-inline');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+var rev = require('gulp-rev');
+const imagemin = require('gulp-imagemin');
+var revCollector = require('gulp-rev-collector');
+
+gulp.task('imagemin', function(cb) {
+  gulp.src('./build/web-mobile/**/*.png')
+      .pipe(imagemin())
+      .pipe(gulp.dest('./build/web-mobile/')
+      .on('end', cb));
+})
+gulp.task('cp-src',['imagemin'], function(cb) {
+  gulp.src(['./build/web-mobile/src/project.js'])
+  .pipe(gulp.dest('./build/web-mobile/')
+  .on('end', cb));
+});
+gulp.task('concat-css', ['cp-src'], function(cb) {
+  gulp.src(['./build/web-mobile/style-mobile.css', './html/loading.css'])
+  .pipe(concat('style-mobile.css'))
+  .pipe(gulp.dest('./build/web-mobile/')
+  .on('end', cb));
+});
+gulp.task('htmlmin', ['concat-css'], function(cb) {
+  gulp.src('./build/web-mobile/*.html')
+  .pipe(fileInline())
+  .pipe(htmlmin({
+      collapseWhitespace:true,
+      removeComments: true
+  }))
+  .pipe(gulp.dest('./build/web-mobile/')
+  .on('end', cb));
+});
+gulp.task('resRev',['htmlmin'], function (cb) {
+  // res, thm资源文件打版本号
+  gulp.src(['./build/web-mobile/**/*.js', './build/web-mobile/*.png'])
+      .pipe(rev())
+      .pipe(gulp.dest('./build/web-mobile/'))
+      .pipe(rev.manifest())
+      .pipe(gulp.dest('./build/web-mobile/')
+      .on('end', cb));
+});
+gulp.task('default',['resRev'], function(cb) {
+  gulp.src(['./build/web-mobile/*.json', './build/web-mobile/index.html'])
+      .pipe(revCollector())
+      .pipe(gulp.dest('./build/web-mobile/'));
+  gulp.src(['./build/web-mobile/*.json', './build/web-mobile/main*.js'])
+      .pipe(revCollector({
+        replaceReved: true
+      }))
+      .pipe(gulp.dest('./build/web-mobile/')); 
+});
+
